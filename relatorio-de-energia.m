e13bca14370ef0bfed2643b1dbab58ebf385b4c9
Anna Chandra <72108588+annacteixeira@users.noreply.github.com>
2024-06-14 15:54:31 -0300

    editing if else structure

--- relatorio-de-energia.m
+++ relatorio-de-energia.m
@@ -8,15 +8,15 @@ consumoFaturado = input("Digite o valor de consumo faturado pela concessionária
 geracaoFaturada = input("Digite o valor de geração faturado pela concessionária: ");
 precokW = input("Digite o valor do kWh: ");
 saldoEnergiaAtual = input("Digite o saldo de geração atual: ");
-saldoEnergiaAntes = input ("Digite o saldo de energia do período anterior: ");
+saldoEnergiaAntes = input("Digite o saldo de energia do período anterior: ");
 
 % Dados de monitoramento
 disp("Inserir dados de monitoramento: ");
 geracaoInversor = input("Digite a geração: ");
 energiaProjetada = input("Digite a energia projetada para o período: ");
 
-%Cálculo desempenho
-desempenho = (((energiaProjetada/geracaoInversor)*100)+100);
+% Cálculo desempenho
+desempenho = (((energiaProjetada / geracaoInversor) * 100) + 100);
 
 disp("Desempenho da Usina: ");
 disp(desempenho)
@@ -30,9 +30,9 @@ disp(consumoReal)
 disp("Valor do auto consumo: ");
 disp(AutoConsumo)
 
-%Cálculos
+% Cálculos
 energiaPaga = input("Digite o valor de energia pago na fatura: ");
-PagariaSemUsina = (consumoReal*precokW);
+PagariaSemUsina = (consumoReal * precokW);
 economia = (PagariaSemUsina - energiaPaga);
 
 disp("Valor que o cliente pagaria sem a usina: ");
@@ -41,13 +41,11 @@ disp("Economia: ");
 disp(economia)
 
 if saldoEnergiaAtual < saldoEnergiaAntes
-  disp("Saldo utilizado: ")
-  saldoUtilizado = (saldoEnergiaAntes - saldoEnergiaAtual);
-  disp(saldoUtilizado)
-
-  else
-  disp("Saldo acumulado: ")
-  saldoAcumulado = (saldoEnergiaAtual - saldoEnergiaAntes);
-  disp(saldoAcumulado)
-
-  end
+    disp("Saldo utilizado: ");
+    saldoUtilizado = (saldoEnergiaAntes - saldoEnergiaAtual);
+    disp(saldoUtilizado)
+else
+    disp("Saldo acumulado: ");
+    saldoAcumulado = (saldoEnergiaAtual - saldoEnergiaAntes);
+    disp(saldoAcumulado)
+end
